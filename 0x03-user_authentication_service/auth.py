78425e85d7ab2b39af395b73d9e904956955fb21
HarpLoid <walesii19@gmail.com>
2024-04-26 07:41:30 +0100

    fixed function annotation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from uuid import uuid4
 import bcrypt
 from db import DB
 from user import User
+from typing import Union
 from sqlalchemy.orm.exc import NoResultFound
 
 
@@ -64,7 +65,7 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> User | None:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """
         gets user from session_id
         """
